3639cca255761c41311445ec86c81ed813aed989
Simon Zimmermann <simonz05@gmail.com>
2013-05-14 18:47:25 +0200

    examples

--- example.py
+++ example.py
@@ -1,7 +1,11 @@
+# -*- coding: utf-8 -*-
 import argparse
-import pycallgraph
 import cProfile
 import pstats
+import pycallgraph
+import timeit
+import time
+
 from activity import Activity
 from activity.utils import datetime_to_timestamp, utcnow, s
 
@@ -25,12 +29,18 @@ def create_item(a, user_id, item_id, timestamp):
 def get_item(item_id):
     return items[str(item_id)]
 
+def items_loader(res):
+    items = [get_item(v) for v in res]
+    time.sleep(0.002)
+    return items
+
 items = {}
 
 def example_setup():
+    global a
     a = Activity(
         redis='redis://:@localhost:6379/15',
-        #item_loader=get_item,
+        items_loader=items_loader,
         aggregate=True)
 
     _empty(a)
@@ -46,14 +56,36 @@ def example_setup():
 
     return a
 
-def example_run(a):
+def example_run():
     for i in range(1, 1200):
         a.feed('foo', 1)
+
+def example_timeit_run():
+    a.feed('foo', 1)
+
+def format_time(seconds):
+    v = seconds
+
+    if v * 1000 * 1000 * 1000 < 1000:
+        scale = u'ns'
+        v = int(round(v*1000*1000*1000))
+    elif v * 1000 * 1000 < 1000:
+        scale = u'µs'
+        v = int(round(v*1000*1000))
+    elif v * 1000 < 1000:
+        scale = u'ms'
+        v = int(round(v*1000))
+    else:
+        scale = u'sec'
+        v = int(v)
+
+    return u'{} {}'.format(v, scale)
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(description='example program')
     parser.add_argument('--profile', action='store_true', help='run profiler')
     parser.add_argument('--graph', action='store_true', help='run graph')
+    parser.add_argument('--timeit', type=int, default=0, help='run benchmark')
 
     args = parser.parse_args()
     a = example_setup()
@@ -65,7 +97,15 @@ if __name__ == '__main__':
     if args.graph:
         pycallgraph.start_trace()
 
-    example_run(a)
+    if args.timeit:
+        number = args.timeit
+        res = timeit.Timer(example_timeit_run).repeat(7, number)
+        min_run = min(res)
+        per_loop = min_run/number
+        print u'{} total run'.format(format_time(min_run))
+        print u'{} per/loop'.format(format_time(per_loop))
+    else:
+        example_run()
 
     if args.graph:
         pycallgraph.make_dot_graph('example.png')
